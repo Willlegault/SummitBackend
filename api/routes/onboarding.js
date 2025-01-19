f614482099cbbf21536e2d54c0e06a506ba50991
William Legault <willlegault24@gmail.com>
2025-01-19 00:30:07 -0500

    tiktok:(

--- api/routes/onboarding.js
+++ api/routes/onboarding.js
@@ -5,5 +5,34 @@ const { default: mongoose } = require('mongoose');
 const router = express.Router();
 
 router.post('/initUser', async (req, res) => {
-    
-})
+     const { firstName, lastName, email, income, totalDebt } = req.body;
+
+     try {
+        const existingUser = await User.findOne({ email }); // Check if a user with the given email already exists.
+        if (existingUser) {
+            return res
+                .status(409)
+                .json({ error: 'User with this email already exists' }); // Return a conflict error if the email is already in use.
+    }
+    res.status(201).json({ message: 'User registered successfully' }); 
+}
+});
+
+router.get('/profile', (req, res) => {
+    const { token } = req.cookies;
+    if (token) {
+        jwt.verify(token, jwtSecret, {}, async (err, userData) => { // Verify the token.
+            if (err) {
+              return res.status(401).json({ message: 'Unauthorized' }); // Return unauthorized if token verification fails.
+            }
+            const { name, email, _id } = await User.findById(userData.id); // Fetch user details using the token payload.
+            res.json({
+              name,
+              email,
+              _id,
+            }); // Return the user profile data.
+          });
+        } else {
+          res.status(401).json({ message: 'Unauthorized' }); // Return unauthorized if no token is provided.
+        }
+});
